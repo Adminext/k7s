615e6c00e96a910c4b31dd7ff87ff58f01211c18
Admin_ext <mdgaorui@buaa.edu.cn>
2021-10-28 02:30:49 +0800

    准备了一个webpack-dev-server的空配置

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    open: process.platform === 'darwin',
+    host: '127.0.0.1',
+    port: 8080,
+    proxy: { //配置跨域
+      '/': {
+        target: '', //这里后台的地址模拟的;应该填写你们真实的后台接口
+        ws: true,
+        changOrigin: true, //允许跨域
+        pathRewrite: {
+          '^/': '' //请求的时候使用这个api就可以
+        },
+      },
+    },
+    https: false,
+    hotOnly: false,
+  },
 }
