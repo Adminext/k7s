dd6472545b32c3f06fd58c7120e5534fa22fbdb3
Admin_ext <mdgaorui@buaa.edu.cn>
2021-10-30 16:08:35 +0800

    支持多个镜像标签

--- src/components/Images/FromLocalDialogContent.vue
+++ src/components/Images/FromLocalDialogContent.vue
@@ -13,7 +13,7 @@
         </v-row>
         <v-row>
           <v-col :cols="6">
-            <v-text-field v-model="name" label="镜像名称" clearable></v-text-field>
+            <v-text-field v-model="name" label="镜像名称" clearable :hint="'用英文逗号分割多个tag'"></v-text-field>
           </v-col>
           <v-col :cols="6">
             <v-text-field v-model="version" label="镜像tag" clearable :hint="'留空以使用自定义格式tag'"></v-text-field>
@@ -48,14 +48,20 @@ export default {
     tag() {
       let flag = ':';
       if(this.name === '' || this.version === '') flag = '';
-      return this.name + flag + this.version;
+      let tags = this.name.split(',');
+      for(let i = 0, l = tags.length; i < l; i++) {
+        tags[i] = tags[i] + flag + this.version;
+      }
+      return tags;
     }
   },
   methods: {
     async buildImage() {
       let form = new FormData();
       form.append('dockerfile', this.dockerFile);
-      form.append('tag', this.tag);
+      for(let i = 0, l = this.tag.length; i < l; i++) {
+        form.append('tags',this.tag[i]);
+      }
       this.cardLoading = true;
       await this.$axios.post('/build_image', form).then((res) => {
         if(res === 'build success') this.result = '镜像构建完成';
