ca89090071bdc93be4d2a64483d254e92ba6cd2b
Admin_ext <mdgaorui@buaa.edu.cn>
2022-01-22 22:59:20 +0800

    last modification

--- src/App.vue
+++ src/App.vue
@@ -6,85 +6,50 @@
       dark
     >
       <div class="d-flex align-center">
-        <span style="font-size: 35px; font-weight: bold;">k7s</span>
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
       </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
     </v-app-bar>
-
-     <v-navigation-drawer
-        permanent
-        app
-    >
-      <v-list-item>
-      <v-list-item-content>
-        <v-list-item-title class="text-h6">
-          PaaS Platform
-        </v-list-item-title>
-        <v-list-item-subtitle>
-          software college @ buaa
-        </v-list-item-subtitle>
-      </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-        <v-list
-          dense
-          nav
-        >
-          <v-list-item
-            v-for="item in items"
-            :key="item.title"
-            router :to="item.route"
-          >
-            <v-list-item-icon>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-icon>
-
-            <v-list-item-content>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-    </v-navigation-drawer>
 
     <v-main>
       <router-view/>
     </v-main>
-
   </v-app>
-  
 </template>
 
 <script>
 
 export default {
   name: 'App',
-  data () {
-      return {
-        items: [
-          { title: '镜像管理', icon: 'mdi-view-dashboard', route: '/images'},
-          { title: '容器管理', icon: 'mdi-forum', route: '/containers'},
-          { title: '应用部署', icon: 'mdi-card-multiple', route: '/applications'},
-        ],
-      }
-    },
+
+  data: () => ({
+    //
+  }),
 };
 </script>
-
-<style>
-*::-webkit-scrollbar{
-  width:5px;
-  height:5px;
-}
-*::-webkit-scrollbar-thumb{ /*滚动条里面小方块*/
-  border-radius: 15px;
-  -webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);
-  /*background: #cce2f7;*/
-  background: #999;
-}
-*::-webkit-scrollbar-track {/*滚动条里面轨道*/
-  -webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);
-  border-radius: 10px;
-  background: #EDEDED;
-}
-</style>
